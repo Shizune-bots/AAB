336775bca7b04f547b38499154f9e125a8705cb5
Incredaimaso <159280005+Incredaimaso@users.noreply.github.com>
2025-03-29 06:46:24 +0530

    Update database.py

--- bot/core/database.py
+++ bot/core/database.py
@@ -7,16 +7,14 @@ class MongoDB:
         self.__db = self.__client[database_name]
         self.__animes = self.__db.animes[Var.BOT_TOKEN.split(':')[0]]
 
-    async def getAnime(self, ani_id):
-        botset = await self.__animes.find_one({'_id': ani_id})
-        return botset or {}
+    async def get_anime(self, ani_id):
+        return await self.__animes.find_one({'_id': ani_id}) or {}
 
-    async def saveAnime(self, ani_id, ep, qual, post_id=None):
-        quals = (await self.getAnime(ani_id)).get(ep, {qual: False for qual in Var.QUALS})
-        quals[qual] = True
-        await self.__animes.update_one({'_id': ani_id}, {'$set': {ep: quals}}, upsert=True)
+    async def save_anime(self, ani_id, ep, qual, post_id=None):
+        update_data = {f"{ep}.{qual}": True}
         if post_id:
-            await self.__animes.update_one({'_id': ani_id}, {'$set': {"msg_id": post_id}}, upsert=True)
+            update_data["msg_id"] = post_id
+        await self.__animes.update_one({'_id': ani_id}, {'$set': update_data}, upsert=True)
 
     async def reboot(self):
         await self.__animes.drop()
