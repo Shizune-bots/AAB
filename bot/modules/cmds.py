e161f7361e6e8b9ddb3d27a80068e0ceeea9792b
Incredaimaso <159280005+Incredaimaso@users.noreply.github.com>
2025-03-29 10:01:38 +0530

    Update cmds.py

--- bot/modules/cmds.py
+++ bot/modules/cmds.py
@@ -1,76 +1,113 @@
-from asyncio import sleep as asleep from pyrogram.filters import command, private, user from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup from pyrogram.errors import MessageNotModified
+from asyncio import sleep as asleep
+from pyrogram.filters import command, private, user
+from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
+from pyrogram.errors import MessageNotModified
 
-from bot import bot, bot_loop, Var, ani_cache from bot.core.database import db from bot.core.func_utils import ( decode, is_fsubbed, get_fsubs, editMessage, sendMessage, new_task, convertTime, getfeed ) from bot.core.auto_animes import get_animes from bot.core.reporter import rep
+from bot import bot, bot_loop, Var, ani_cache
+from bot.core.database import db
+from bot.core.func_utils import (
+    decode, is_fsubbed, get_fsubs, editMessage, sendMessage, 
+    new_task, convertTime, getfeed
+)
+from bot.core.auto_animes import get_animes
+from bot.core.reporter import rep
 
-@bot.on_message(command('start') & private) @new_task async def start_msg(client, message): uid = message.from_user.id from_user = message.from_user txtargs = message.text.split() temp = await sendMessage(message, "<i>Connecting..</i>")
 
-if not await is_fsubbed(uid):
-    txt, btns = await get_fsubs(uid, txtargs)
-    return await editMessage(temp, txt, InlineKeyboardMarkup(btns))
+@bot.on_message(command('start') & private)
+@new_task
+async def start_msg(client, message):
+    uid = message.from_user.id
+    from_user = message.from_user
+    txtargs = message.text.split()
+    temp = await sendMessage(message, "<i>Connecting..</i>")
 
-if len(txtargs) == 1:
-    await temp.delete()
-    buttons = [
-        [InlineKeyboardButton(bt, url=link)]
-        for elem in Var.START_BUTTONS.split()
-        if (bt := elem.split('|', maxsplit=1)[0]) and (link := elem.split('|', maxsplit=1)[1])
-    ]
+    if not await is_fsubbed(uid):
+        txt, btns = await get_fsubs(uid, txtargs)
+        return await editMessage(temp, txt, InlineKeyboardMarkup(btns))
 
-    smsg = Var.START_MSG.format(
-        first_name=from_user.first_name, last_name=from_user.last_name,
-        mention=from_user.mention, user_id=from_user.id
-    )
-
-    if Var.START_PHOTO:
-        await message.reply_photo(photo=Var.START_PHOTO, caption=smsg, reply_markup=InlineKeyboardMarkup(buttons) if buttons else None)
-    else:
-        await sendMessage(message, smsg, InlineKeyboardMarkup(buttons) if buttons else None)
-    return
-
-try:
-    arg = (await decode(txtargs[1])).split('-')
-    if len(arg) == 2 and arg[0] == 'get':
-        fid = int(int(arg[1]) / abs(int(Var.FILE_STORE)))
-        msg = await client.get_messages(Var.FILE_STORE, message_ids=fid)
-        if msg.empty:
-            return await editMessage(temp, "<b>File Not Found !</b>")
-
-        nmsg = await msg.copy(message.chat.id, reply_markup=None)
+    if len(txtargs) == 1:
         await temp.delete()
+        buttons = [
+            [InlineKeyboardButton(bt, url=link)]
+            for elem in Var.START_BUTTONS.split()
+            if (bt := elem.split('|', maxsplit=1)[0]) and (link := elem.split('|', maxsplit=1)[1])
+        ]
 
-        if Var.AUTO_DEL:
-            async def auto_del(msg, timer):
-                await asleep(timer)
-                await msg.delete()
-            bot_loop.create_task(auto_del(nmsg, Var.DEL_TIMER))
-            await sendMessage(message, f'<i>File will be Auto Deleted in {convertTime(Var.DEL_TIMER)}, Forward to Saved Messages Now..</i>')
+        smsg = Var.START_MSG.format(
+            first_name=from_user.first_name, last_name=from_user.last_name,
+            mention=from_user.mention, user_id=from_user.id
+        )
+
+        if Var.START_PHOTO:
+            await message.reply_photo(photo=Var.START_PHOTO, caption=smsg, reply_markup=InlineKeyboardMarkup(buttons) if buttons else None)
+        else:
+            await sendMessage(message, smsg, InlineKeyboardMarkup(buttons) if buttons else None)
         return
-except Exception as e:
-    await rep.report(f"User : {uid} | Error : {str(e)}", "error")
 
-await editMessage(temp, "<b>Input Link is Invalid for Usage !</b>")
+    try:
+        arg = (await decode(txtargs[1])).split('-')
+        if len(arg) == 2 and arg[0] == 'get':
+            fid = int(int(arg[1]) / abs(int(Var.FILE_STORE)))
+            msg = await client.get_messages(Var.FILE_STORE, message_ids=fid)
+            if msg.empty:
+                return await editMessage(temp, "<b>File Not Found !</b>")
 
-@bot.on_message(command(['pause', 'resume']) & private & user(Var.ADMINS)) async def toggle_fetch(client, message): action = message.command[0] ani_cache['fetch_animes'] = action == 'resume' await sendMessage(message, f"Successfully {'Resumed' if action == 'resume' else 'Paused'} Fetching Animes...")
+            nmsg = await msg.copy(message.chat.id, reply_markup=None)
+            await temp.delete()
 
-@bot.on_message(command('log') & private & user(Var.ADMINS)) @new_task async def _log(client, message): await message.reply_document("log.txt", quote=True)
+            if Var.AUTO_DEL:
+                async def auto_del(msg, timer):
+                    await asleep(timer)
+                    await msg.delete()
+                bot_loop.create_task(auto_del(nmsg, Var.DEL_TIMER))
+                await sendMessage(message, f'<i>File will be Auto Deleted in {convertTime(Var.DEL_TIMER)}, Forward to Saved Messages Now..</i>')
+            return
+    except Exception as e:
+        await rep.report(f"User : {uid} | Error : {str(e)}", "error")
 
-@bot.on_message(command('addlink') & private & user(Var.ADMINS)) @new_task async def add_link(client, message): args = message.text.split(maxsplit=1) if len(args) <= 1: return await sendMessage(message, "<b>No Link Found to Add</b>")
+    await editMessage(temp, "<b>Input Link is Invalid for Usage !</b>")
 
-Var.RSS_ITEMS.append(args[1])
-await sendMessage(message, f"""`Global Link Added Successfully!`
+
+@bot.on_message(command(['pause', 'resume']) & private & user(Var.ADMINS))
+async def toggle_fetch(client, message):
+    action = message.command[0]
+    ani_cache['fetch_animes'] = action == 'resume'
+    await sendMessage(message, f"Successfully {'Resumed' if action == 'resume' else 'Paused'} Fetching Animes...")
+
+
+@bot.on_message(command('log') & private & user(Var.ADMINS))
+@new_task
+async def _log(client, message):
+    await message.reply_document("log.txt", quote=True)
+
+
+@bot.on_message(command('addlink') & private & user(Var.ADMINS))
+@new_task
+async def add_link(client, message):
+    args = message.text.split(maxsplit=1)
+    if len(args) <= 1:
+        return await sendMessage(message, "<b>No Link Found to Add</b>")
+
+    Var.RSS_ITEMS.append(args[1])
+    await sendMessage(message, f"""`Global Link Added Successfully!`
 
 • **All Link(s) :** {', '.join(Var.RSS_ITEMS)}""")
 
-@bot.on_message(command('addtask') & private & user(Var.ADMINS)) @new_task async def add_task(client, message): args = message.text.split() if len(args) <= 1: return await sendMessage(message, "<b>No Task Found to Add</b>")
 
-index = int(args[2]) if len(args) > 2 and args[2].isdigit() else 0
-taskInfo = await getfeed(args[1], index)
-if not taskInfo:
-    return await sendMessage(message, "<b>No Task Found to Add for the Provided Link</b>")
+@bot.on_message(command('addtask') & private & user(Var.ADMINS))
+@new_task
+async def add_task(client, message):
+    args = message.text.split()
+    if len(args) <= 1:
+        return await sendMessage(message, "<b>No Task Found to Add</b>")
 
-bot_loop.create_task(get_animes(taskInfo.title, taskInfo.link, True))
-await sendMessage(message, f"""<i><b>Task Added Successfully!</b></i>
+    index = int(args[2]) if len(args) > 2 and args[2].isdigit() else 0
+    taskInfo = await getfeed(args[1], index)
+    if not taskInfo:
+        return await sendMessage(message, "<b>No Task Found to Add for the Provided Link</b>")
+
+    bot_loop.create_task(get_animes(taskInfo.title, taskInfo.link, True))
+    await sendMessage(message, f"""<i><b>Task Added Successfully!</b></i>
 
 • <b>Task Name :</b> {taskInfo.title}
 • <b>Task Link :</b> {args[1]}""")
-
