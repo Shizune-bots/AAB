3be7bb397a4c2d606abb60c7b6de2653de6c03c9
Incredaimaso <159280005+Incredaimaso@users.noreply.github.com>
2025-03-30 12:51:39 +0530

    Update database.py

--- bot/core/database.py
+++ bot/core/database.py
@@ -6,6 +6,7 @@ class MongoDB:
         self.__client = AsyncIOMotorClient(uri)
         self.__db = self.__client[database_name]
         self.__animes = self.__db.animes[Var.BOT_TOKEN.split(':')[0]]
+        self.__channels = self.__db.channels  # New collection for anime-channel mappings
 
     async def getAnime(self, ani_id):
         botset = await self.__animes.find_one({'_id': ani_id})
@@ -20,5 +21,28 @@ class MongoDB:
 
     async def reboot(self):
         await self.__animes.drop()
+
+    # New Functions for Separate Channel Mapping
+    async def set_separate_channel(self, anime_name, channel_id):
+        """Set a separate upload channel for a specific anime."""
+        await self.__channels.update_one(
+            {'anime_name': anime_name.lower()},
+            {'$set': {'channel_id': channel_id}},
+            upsert=True
+        )
+
+    async def get_separate_channel(self, anime_name):
+        """Retrieve the upload channel for a specific anime."""
+        data = await self.__channels.find_one({'anime_name': anime_name.lower()})
+        return data['channel_id'] if data else None
+
+    async def remove_separate_channel(self, anime_name):
+        """Remove a channel mapping for an anime."""
+        await self.__channels.delete_one({'anime_name': anime_name.lower()})
+
+    async def get_all_separate_channels(self):
+        """Return all anime-to-channel mappings."""
+        mappings = await self.__channels.find().to_list(length=None)
+        return {item['anime_name']: item['channel_id'] for item in mappings}
 
 db = MongoDB(Var.MONGO_URI, "FZAutoAnimes")
